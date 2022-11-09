321a7563c12123aeaa0d4091e6273e94c6db597c
wilson-bg <w.beltran@uniandes.edu.co>
2022-11-08 22:29:36 -0500

    Init test ProcessBatchAsync

--- Redbridge.WebApi.Tests/TestCompressingHttpBatchHandler.cs
+++ Redbridge.WebApi.Tests/TestCompressingHttpBatchHandler.cs
@@ -16,5 +16,10 @@ namespace Redbridge.WebApi.Tests
     [TestFixture]
     public class TestCompressingHttpBatchHandler
     {
+        [Test]
+        public void ProcessBatchAsync()
+        {
+            Assert.NotNull(1);
+        }
     }
 }
